0a98cf615f5e9eb7c214fe27bcb2611f031fc842
Carl Vuorinen <carl.vuorinen@gmail.com>
2015-04-12 12:23:31 +0300

    Refactor generated code and remove unnecessary stuff

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,6 @@
-'use strict';
+(function() {
+    'use strict';
 
-angular
-  .module('reactiveLifeApp', []);
+    angular
+        .module('app', []);
+})();
---
+++ app/scripts/controllers.js
@@ -0,0 +1,13 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('MainCtrl', MainCtrl);
+
+    function MainCtrl() {
+        var vm = this;
+
+
+    }
+})();
